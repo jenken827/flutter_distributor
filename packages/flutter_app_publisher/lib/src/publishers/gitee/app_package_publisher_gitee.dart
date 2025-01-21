ff05adcbfa82cf48a7437015253d991b47de61ff
jenken827 <jenken827@gmail.com>
2025-01-21 14:32:56 +0800

    fix:null safe

--- packages/flutter_app_publisher/lib/src/publishers/gitee/app_package_publisher_gitee.dart
+++ packages/flutter_app_publisher/lib/src/publishers/gitee/app_package_publisher_gitee.dart
@@ -81,7 +81,7 @@ class AppPackagePublisherGitee extends AppPackagePublisher {
     Response resp = await Dio().get(
       'https://gitee.com/api/v5/repos/${pconfig.repoOwner}/${pconfig.repoName}/releases/tags/$tagname',
     );
-    return resp.data['id'];
+    return resp.data?['id'];
   }
 
   /// Create release
